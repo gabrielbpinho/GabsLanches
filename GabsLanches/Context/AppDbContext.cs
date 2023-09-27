7f6b2182ae83d8b86764e9102714510398d3caf9
Gabriel Pinho <gabrielbarbosapinho@gmail.com>
2023-09-26 23:47:00 -0300

    Criando o modelo de dominio Apresentando o EF Core Configurando EF Core

---
+++ GabsLanches/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GabsLanches.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GabsLanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
