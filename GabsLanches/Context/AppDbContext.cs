a849020af46246d8286f74ecb52bf7cf70b4ad4d
Gabriel Pinho <gabrielbarbosapinho@gmail.com>
2024-01-02 23:05:57 -0300

    Subindo Taghelpers Carrinho de compra Intens do carrinho de compras

--- GabsLanches/Context/AppDbContext.cs
+++ GabsLanches/Context/AppDbContext.cs
@@ -9,5 +9,6 @@ namespace GabsLanches.Context
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
     }
 }
---
+++ GabsLanches/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using GabsLanches.Models;
+
+namespace GabsLanches.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+    }
+}
