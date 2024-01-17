9c5a8fe7dc245ef6334cbb67eee705c8678d4ec3
Gabriel Pinho <gabrielbarbosapinho@gmail.com>
2024-01-16 23:09:14 -0300

    Pedido PedidoDetalhe

--- GabsLanches/Context/AppDbContext.cs
+++ GabsLanches/Context/AppDbContext.cs
@@ -10,5 +10,7 @@ namespace GabsLanches.Context
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
     }
 }
