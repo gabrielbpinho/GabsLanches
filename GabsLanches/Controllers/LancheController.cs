1c0db5a9d6259e670d694f2880afa3d230137798
Gabriel Pinho <gabrielbarbosapinho@gmail.com>
2024-01-19 22:15:29 -0300

    Barra de busca

--- GabsLanches/Controllers/LancheController.cs
+++ GabsLanches/Controllers/LancheController.cs
@@ -54,6 +54,34 @@ namespace GabsLanches.Controllers
             return View(lanche);
         }
 
+        public ViewResult Search(string searchString)
+        {
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                lanches = _lancheRepository.GetLanches.OrderBy(l => l.Nome);
+                categoriaAtual = "Todas os laches";
+            }
+            else 
+            {
+                lanches = _lancheRepository.GetLanches.Where(l => l.Nome.ToLower().Contains(searchString.ToLower()));
+
+                if (lanches.Any())
+
+                    categoriaAtual = "Lanches";
+                else
+                    categoriaAtual = "Nenhum lanche foi encontrado";
+            }
+
+            return View("~/Views/Lanche/List.cshtml", new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            });
+        }
+
     }
     
 }
